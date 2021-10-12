056d3bc4b42e610abe4f05a19609a5d401d6349f
Caique Cezar <cezar.caique@gmail.com>
2021-10-11 22:44:36 -0300

    add new requirement: special days

--- app/Models/Calendar/Calendar.php
+++ app/Models/Calendar/Calendar.php
@@ -2,29 +2,67 @@
 
 namespace App\Models\Calendar;
 
-use App\Models\Weekdays\Sunday;
-use App\Models\Weekdays\Monday;
-use App\Models\Weekdays\Tuesday;
-use App\Models\Weekdays\Wednesday;
-use App\Models\Weekdays\Thursday;
-use App\Models\Weekdays\Friday;
-use App\Models\Weekdays\Saturday;
-
+use App\Interfaces\Weekday;
 use DateTime;
-use Exception;
 
 class Calendar
 {
-    public function getWeekday($date, $format = 'Y-m-d')
-    {
-        $created_date = DateTime::createFromFormat($format, $date);
+    protected $special_dates;
 
-        if (!$created_date) {
-            throw new Exception('O formato da data não é válido');
+    public function __construct()
+    {
+        $this->special_dates = [];
+    }
+
+    /**
+     * Function to get a weekday according with date
+     * 
+     * @param Datetime $date
+     * 
+     * @return Weekday
+     */
+    public function getWeekday(Datetime $date)
+    {
+        $special_date = $this->findSpecialDate($date);
+
+        if ($special_date) {
+            return $special_date;
         }
 
-        $weekday = $created_date->format('l');
+        $weekday = $date->format('l');
         $class = 'App\\Models\\Weekdays\\' . $weekday;
-        return new $class();
+        return new $class($date);
+    }
+
+    /**
+     * Function to add a special date in calendar
+     * 
+     * @param SpecialDate $special_date special date to add
+     * 
+     * @return Calendar own instance
+     */
+    public function addSpecialDate(SpecialDate $special_date)
+    {
+        $this->special_dates[] = $special_date;
+
+        return $this;
+    }
+
+    /**
+     * Function to search for a special date
+     * 
+     * @param Datetime $date date to search
+     * 
+     * @return SpecialDate|false if cant find, return false
+     */
+    private function findSpecialDate(Datetime $date)
+    {
+        foreach ($this->special_dates as $special_date) {
+            if ($date->format('Y-m-d') === $special_date->getDate()) {
+                return $special_date;
+            }
+        }
+
+        return false;
     }
 }
