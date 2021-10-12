7ff0307d16b4a3b6788a63ac6b295ebdebbd25e3
Caique Cezar <cezar.caique@gmail.com>
2021-10-12 12:28:41 -0300

    add code sample

--- app/index.php
+++ app/index.php
@@ -3,3 +3,24 @@
 namespace App;
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+/* Exemplo de código */
+
+use App\Models\Calendar\Calendar;
+use App\Models\Calendar\SpecialDate;
+use DateTime;
+
+$calendar = new Calendar();
+
+$date = DateTime::createFromFormat('Y-m-d', '2018-01-21');
+$special_date = new SpecialDate($date, 'Um dia muito especial.');
+
+$calendar->addSpecialDate($special_date);
+
+$current_date = new DateTime('now');
+$weekday = $calendar->getWeekday($current_date);
+echo $weekday->message() . PHP_EOL;
+
+$special_date = DateTime::createFromFormat('Y-m-d', '2018-01-21');
+$weekday = $calendar->getWeekday($special_date);
+echo $weekday->message() . PHP_EOL;
