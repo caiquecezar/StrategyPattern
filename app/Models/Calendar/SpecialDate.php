cc4bc92d31991cf79df3399cf75474a68c642f83
Caique Cezar <cezar.caique@gmail.com>
2021-10-11 23:34:52 -0300

    add iterator to be possible to have more than one special date in same day

--- app/Models/Calendar/SpecialDate.php
+++ app/Models/Calendar/SpecialDate.php
@@ -4,21 +4,22 @@ namespace App\Models\Calendar;
 
 use App\Interfaces\Weekday;
 use DateTime;
-use Exception;
 
 class SpecialDate implements Weekday
 {
     protected Datetime $date;
     protected Weekday $weekday;
     protected string $message;
+    protected ?SpecialDate $next_special_date;
 
     public function __construct(Datetime $date, string $message)
     {
+        $this->next_special_date = null;
         $this->date = $date;
         $this->message = $message;
 
         $weekday = $date->format('l');
-        $class = 'App\\Models\\Weekdays\\' . $weekday;
+        $class = 'App\\Models\\WeekdayStrategy\\' . $weekday;
         $this->weekday = new $class($date);
     }
 
@@ -29,11 +30,40 @@ class SpecialDate implements Weekday
      */
     public function getDate()
     {
-        return $this->date->format('Y-m-d');
+        return $this->date;
+    }
+
+    /**
+     * Function to add message
+     * 
+     * @param SpecialDate $special_date
+     * 
+     * @return SpecialDate own instance
+     */
+    public function addSpecialDate(SpecialDate $special_date)
+    {
+        if ($this->next_special_date == null) {
+            $this->next_special_date = $special_date;
+        } else {
+            $this->next_special_date->addSpecialDate($special_date);
+        }
+
+        return $this;
+    }
+
+    public function getAllMessages()
+    {
+        if ($this->next_special_date == null) {
+            return $this->message . PHP_EOL;
+        }
+
+        return $this->message . PHP_EOL . $this->next_special_date->getAllMessages();
     }
 
     public function message()
     {
-        return $this->weekday->message() . $this->message . PHP_EOL;
+        $final_message = $this->weekday->message() . $this->getAllMessages();
+
+        return $final_message;
     }
 }
